63bcbbc9616e066cec1213a65de1c6bdbdd58ac0
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-23 18:05:32 +0900

    New: Error > _Session.php

---
+++ Error/_Session.php
@@ -0,0 +1,25 @@
+<?php
+/**	op-core-ci:/Error/_Session.php
+ *
+ * @created    2025-06-23
+ * @version    1.0
+ * @package    op-core
+ * @subpackage ci
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci \OP\UNIT\CI\CI_Config */
+
+//	...
+$method = '_Session';
+
+//	...
+$result = [];
+$args   = null;
+$ci->Set($method, $result, $args);
