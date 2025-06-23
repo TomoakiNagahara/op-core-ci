2917c2a4a7aaf58e7e2860dae6c4c5b63acf4c42
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-23 18:06:29 +0900

    New: Error > Notice.php

---
+++ Error/Notice.php
@@ -0,0 +1,25 @@
+<?php
+/**	op-core-ci:/Error/Notice.php
+ *
+ * @created    2025-06-23
+ * @version    1.0
+ * @package    op-core
+ * @subpackage ci
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci \OP\UNIT\CI\CI_Config */
+
+//	...
+$method = 'Notice';
+
+//	...
+$result = null;
+$args   = null;
+$ci->Set($method, $result, $args);
