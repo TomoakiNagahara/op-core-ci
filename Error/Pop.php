a171f2d8cf24338acaa22fb6cadc328ddd8a34a7
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-23 18:06:20 +0900

    New: Error > Pop.php

---
+++ Error/Pop.php
@@ -0,0 +1,25 @@
+<?php
+/**	op-core-ci:/Error/Pop.php
+ *
+ * @created    2025-06-23
+ * @version    1.0
+ * @package    op-core
+ * @subpackage ci
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci \OP\UNIT\CI\CI_Config */
+
+//	...
+$method = 'Pop';
+
+//	...
+$result = [];
+$args   = null;
+$ci->Set($method, $result, $args);
