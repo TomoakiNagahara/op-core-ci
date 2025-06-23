0be4da204df2b8627024db20c376836519983213
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-23 18:06:11 +0900

    New: Error > Set.php

---
+++ Error/Set.php
@@ -0,0 +1,30 @@
+<?php
+/**	op-core-ci:/Error/Set.php
+ *
+ * @created    2025-06-23
+ * @version    1.0
+ * @package    op-core
+ * @subpackage ci
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci \OP\UNIT\CI\CI_Config */
+
+//	...
+$method = 'Set';
+
+//	...
+$result = 'Notice: (empty)';
+$args   = null;
+$ci->Set($method, $result, $args);
+
+//	...
+$result = 'Notice: This is error test.';
+$args   = 'This is error test.';
+$ci->Set($method, $result, $args);
