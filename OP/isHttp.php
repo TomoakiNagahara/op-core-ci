95857538cc08aec2bce4ef345e93bbb3c5741983
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-29 16:21:38 +0900

    New: OP > isHttp.php, isHTTPs.php

---
+++ OP/isHttp.php
@@ -0,0 +1,25 @@
+<?php
+/**	op-core-ci:/OP/isHTTPs.php
+ *
+ * @created    2025-06-29
+ * @version    1.0
+ * @package    op-core
+ * @subpackage ci
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci \OP\UNIT\CI\CI_Config */
+
+//	...
+$method = 'isHTTPs';
+
+//	...
+$result = false;
+$args   = null;
+$ci->Set($method, $result, $args);
