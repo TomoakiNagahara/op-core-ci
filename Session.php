367400d53ec23dc6d84c2d1328cde70dc4087973
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-30 01:46:45 +0900

    New: Session.php

---
+++ Session.php
@@ -0,0 +1,41 @@
+<?php
+/**	op-core-ci:/Session.php
+ *
+ * @created    2022-11-01
+ * @version    1.0
+ * @package    op-core
+ * @subpackage ci
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci \OP\UNIT\CI\CI_Config */
+$ci = OP()->Unit()->CI()->Config();
+
+//	Get
+$args   = 'self-check';
+$result = null;
+$ci->Set('Get', $result, $args);
+
+//	Set
+$args   = ['self-check', true];
+$result = null;
+$ci->Set('Set', $result, $args);
+
+//	Session
+$args   = null;
+$result = ['self-check'=>true];
+$ci->Set('Session', $result, $args);
+
+//	...
+return $ci->Get();
