d6d9b4176527bacc6cf1e662eebe11ccfb89a3ff
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-30 01:40:01 +0900

    New: OP > URL.php

---
+++ OP/URL.php
@@ -0,0 +1,30 @@
+<?php
+/**	op-core-ci:/OP/isCI.php
+ *
+ * @created    2025-06-23
+ * @version    1.0
+ * @package    op-core
+ * @subpackage ci
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci \OP\UNIT\CI\CI_Config */
+
+//	...
+$method = 'URL';
+
+//	Meta path
+$args   = 'app:/foo/bar/';
+$result = '/foo/bar/';
+$ci->Set($method, $result, $args);
+
+//	Full path
+$args   = _ROOT_APP_.'/foo/bar/';
+$result = '/foo/bar/';
+$ci->Set($method, $result, $args);
