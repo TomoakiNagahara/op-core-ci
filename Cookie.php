8ba11157774b537421ead058256bcfa6e9e6f6c5
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-29 17:13:21 +0900

    New: Cookie.php

---
+++ Cookie.php
@@ -0,0 +1,42 @@
+<?php
+/**	op-core-ci:/Cookie.php
+ *
+ * @created    2022-10-31
+ * @version    1.0
+ * @package    op-core
+ * @subpackage ci
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci \OP\UNIT\CI\CI_Config */
+$ci = OP()->Unit()->CI()->Config();
+
+//	Get
+$result = 'Notice: Cookie can not be used in the shell environment.';
+$args   =  null;
+$ci->Set('Get', $result, $args);
+
+//	Set
+$result = 'Notice: Cookie can not be used in the shell environment.';
+$args   = ['count','1'];
+$ci->Set('Set', $result, $args);
+
+//	...
+$method = 'UserID';
+$result =  null;
+$args   =  null;
+$ci->Set($method, $result, $args);
+
+//	...
+return $ci->Get();
