c5a748e024cee345c53827e82fc1fd63170c33de
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-07-04 18:39:59 +0900

    New: Env > AppID.php | Positive: Get AppID, Negative: Overwrite AppID

---
+++ Env/AppID.php
@@ -0,0 +1,30 @@
+<?php
+/**	op-core-ci:/Env/AppID.php
+ *
+ * @created    2025-06-23
+ * @version    1.0
+ * @package    op-core
+ * @subpackage ci
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/* @var $ci UNIT\CI\CI_Config */
+
+//	...
+$method = 'AppID';
+
+//	Positive: Get AppID
+$result = 'CI';
+$args   =  null;
+$ci->Set($method, $result, $args);
+
+//	Negative: Overwrite AppID
+$result = 'Exception: AppID can no longer be assigned: hoge';
+$args   = 'hoge';
+$ci->Set($method, $result, $args);
